eaca914eb7572e79b8bb263432a082ccd885e2ab
sya-ri <sya79lua@gmail.com>
2020-05-04 23:17:32 +0900

    WeaponType を SS-Item に移動

--- src/main/kotlin/me/syari/ss/job/grade/grade1/Warrior.kt
+++ src/main/kotlin/me/syari/ss/job/grade/grade1/Warrior.kt
@@ -3,7 +3,7 @@ package me.syari.ss.job.grade.grade1
 import me.syari.ss.battle.status.player.StatusChange.Type.Add
 import me.syari.ss.battle.status.player.StatusChange.Type.Multi
 import me.syari.ss.battle.status.player.StatusType.BaseAttack
-import me.syari.ss.job.WeaponType
+import me.syari.ss.item.weapon.WeaponType
 import me.syari.ss.job.grade.JobData
 import me.syari.ss.job.grade.Requirement
 import me.syari.ss.job.skill.active.base.ActiveBaseAttackUp
