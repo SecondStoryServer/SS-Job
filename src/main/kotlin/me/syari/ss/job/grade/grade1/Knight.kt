c6b284f09aee23d565a3b72f102bee40f49ea391
sya-ri <sya79lua@gmail.com>
2020-05-12 22:41:30 +0900

    関数の直接インポートに変更

--- src/main/kotlin/me/syari/ss/job/grade/grade1/Knight.kt
+++ src/main/kotlin/me/syari/ss/job/grade/grade1/Knight.kt
@@ -7,7 +7,7 @@ import me.syari.ss.item.equip.weapon.WeaponType
 import me.syari.ss.job.grade.JobData
 import me.syari.ss.job.grade.Requirement
 import me.syari.ss.job.skill.active.ActiveSkillBuilder.Companion.buildActiveSkill
-import me.syari.ss.job.skill.passive.PassiveSkillBuilder
+import me.syari.ss.job.skill.passive.PassiveSkillBuilder.Companion.buildPassiveSkill
 import org.bukkit.Material
 
 object Knight: JobData {
@@ -24,7 +24,7 @@ object Knight: JobData {
     override val activeSkill = buildActiveSkill {
 
     }
-    override val passiveSkill = PassiveSkillBuilder.buildPassiveSkill {
+    override val passiveSkill = buildPassiveSkill {
         add(1, StatusType.BaseDefense, 3F, Add)
         add(5, StatusType.BaseDefense, 3F, Add)
         add(10, StatusType.MaxHealth, 4F, Add)
