79bfd8e30b09d3f3a3b4de9d32f54214bfedf9a6
sya-ri <sya79lua@gmail.com>
2020-07-25 15:42:47 +0900

    SS-Core v3.1 に対応

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ repositories {
 
 dependencies {
     implementation("com.destroystokyo.paper:paper-api:1.16.1-R0.1-SNAPSHOT")
-    implementation("me.syari.ss.core:SS-Core:3.0")
+    implementation("me.syari.ss.core:SS-Core:3.1")
     implementation("me.syari.ss.battle:SS-Battle:1.0")
     implementation("me.syari.ss.item:SS-Item:1.0")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
